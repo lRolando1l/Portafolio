8bdbafa062c223689185f0f99102a9da2a2c27b1
Rolando Soriano <edutrujillo94@gmail.com>
2026-03-26 13:02:35 -0500

    segundo avanze de portafolio

--- Portafolio.js
+++ Portafolio.js
@@ -35,3 +35,29 @@ window.addEventListener("scroll", () => {
 aboutSection.style.opacity = 0;
 aboutSection.style.transform = "translateY(50px)";
 aboutSection.style.transition = "0.6s";
+
+const projectCards = document.querySelectorAll(".project-card");
+
+projectCards.forEach(card => {
+  card.addEventListener("click", () => {
+    projectCards.forEach(c => {
+      if (c !== card) c.classList.remove("active");
+    });
+    card.classList.toggle("active");
+  });
+});
+const expItem = document.querySelector(".experience-item");
+const savedState = localStorage.getItem("acordeonAbierto");
+if (savedState === null || savedState === "true") {
+  expItem.classList.add("active");
+}
+expItem.addEventListener("click", () => {
+  expItem.classList.toggle("active");
+  const estaAbierto = expItem.classList.contains("active");
+  localStorage.setItem("acordeonAbierto", estaAbierto);
+});
+document.querySelectorAll(".exp-card").forEach(card => {
+  card.addEventListener("click", (e) => {
+    e.stopPropagation();
+  });
+});
